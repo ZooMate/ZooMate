bc67affa22f8a33497f161dd0db5c313f5167e60
ksiomng <nadana0929@gmail.com>
2025-05-22 11:17:40 +0900

    프로젝트 파일 추가

---
+++ ZooMate/ZooMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZooMateApp.swift
+//  ZooMate
+//
+//  Created by Song Kim on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ZooMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
