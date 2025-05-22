1a7fad0c26fabcb75befdfaddb6d4e3ba0c65c9f
Qnokka <106862472+Qnokka@users.noreply.github.com>
2025-05-22 18:20:51 +0900

    데이터베이스 테이블 설계 변경
    회원가입 방식 변경에 따른 테이블 설계 변경

--- ZooMate/Model/User.swift
+++ ZooMate/Model/User.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 // MARK: - 사용자 정보
 struct User: Codable {
-    let userId: String             // 사용자 고유 ID
-    var userName: String           // 사용자 이름
+    let userId: String              // 사용자 고유 ID
+    var userName: String            // 사용자 이름
+    var password: String            // 사용자 암호
     var region: String?             // 사용자 지역(구)
     var desc: String?               // 사용자 소개글
     var pets: [Pet]?                // 사용자가 보유한 반려동물 목록
