0870e6ad3dabbe4d4de0c1e13ed29ebbccf2270b
ksiomng <nadana0929@gmail.com>
2025-05-22 11:35:53 +0900

    chore: 컬러에셋 추가

---
+++ ZooMate/Extension+Color.swift
@@ -0,0 +1,16 @@
+//
+//  Extension+Color.swift
+//  ZooMate
+//
+//  Created by Song Kim on 5/22/25.
+//
+
+import SwiftUI
+
+extension Color {
+    static let backgroundWhite = Color("Background")
+    static let sandBeige = Color("Beige")
+    static let textBrown = Color("DarkBrown")
+    static let ligthBrown = Color("LigthBrown")
+    static let pointPink = Color("PinkPoint")
+}
