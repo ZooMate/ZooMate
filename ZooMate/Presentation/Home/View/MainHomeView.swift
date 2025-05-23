09c4590d74d815217cb06ba9c4e4c6b321de3960
ksiomng <nadana0929@gmail.com>
2025-05-23 16:12:21 +0900

    Feat: 네비게이션 추가

---
+++ ZooMate/Presentation/Home/View/MainHomeView.swift
@@ -0,0 +1,90 @@
+//
+//  MainHomeView.swift
+//  ZooMate
+//
+//  Created by Song Kim on 5/23/25.
+//
+
+import SwiftUI
+
+struct MainHomeView: View {
+    @State var textMenu: String = "전체지역"
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.background
+                    .ignoresSafeArea()
+                
+                VStack {
+                    CategoryTabView()
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Menu {
+                            Button {
+                                self.textMenu = "전체지역"
+                            } label: {
+                                Label {
+                                    Text("전체지역")
+                                } icon: {
+                                    if textMenu == "전체지역" {
+                                        Image(systemName: "checkmark")
+                                    }
+                                }
+                            }
+                            
+                            Button {
+                                self.textMenu = "강동구"
+                            } label: {
+                                Label {
+                                    Text("강동구")
+                                } icon: {
+                                    if textMenu == "강동구" {
+                                        Image(systemName: "checkmark")
+                                    }
+                                }
+                            }
+                            
+                            Button {
+                                // 지역 선택하기 액션
+                            } label: {
+                                Text("지역 선택하기")
+                            }
+                        } label: {
+                            HStack {
+                                Text(textMenu)
+                                Image(systemName: "chevron.down")
+                                    .resizable()
+                                    .frame(width: 14, height: 10)
+                            }
+                        }
+                        .font(.notoSansBold(size: 25))
+                        .foregroundStyle(.mainText)
+                    }
+                    
+                    ToolbarItemGroup(placement: .navigationBarTrailing) {
+                        Button {
+                            // 액션
+                        } label: {
+                            Image(systemName: "magnifyingglass")
+                                .foregroundStyle(.mainText)
+                        }
+                        .frame(width: 20)
+                        
+                        Button {
+                            // 액션
+                        } label: {
+                            Image(systemName: "bell")
+                                .foregroundStyle(.mainText)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+#Preview {
+    MainHomeView()
+}
